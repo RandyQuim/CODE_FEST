867f05ab921c7830c2cca8f2c4333f012687b6eb
Louis Anthony <lja8@students.uwf.edu>
2018-04-07 11:18:41 -0500

    Added initial getter/setter to student class

--- Neural Pathways/src/TeacherAdministration.java
+++ Neural Pathways/src/TeacherAdministration.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
 
 public class TeacherAdministration {
 
+	private ArrayList<Student> students;
+	
+	public TeacherAdministration() {
+		this.students = new ArrayList<Student>();
+	}
+	
 }
