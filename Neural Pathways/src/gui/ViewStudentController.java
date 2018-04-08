1aa7ca9a1150d4b58a42f0881dad9f74d4476357
Louis Anthony <lja8@students.uwf.edu>
2018-04-08 00:26:15 -0500

    Added initial method set to View Student Controller

--- Neural Pathways/src/gui/ViewStudentController.java
+++ Neural Pathways/src/gui/ViewStudentController.java
@@ -1,6 +1,9 @@
 package gui;
 
+import controller.TeacherAdministration;
+import controller.TestMain;
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
 
 import javafx.scene.control.Button;
 
@@ -17,5 +20,25 @@ public class ViewStudentController {
 	private Label nameLabel;
 	@FXML
 	private Label scoreLabel;
+	
+	TeacherAdministration teacher;
+	private Stage dialogStage;
+	private boolean okClicked = false;
+	
+	private void initialize() {
+		
+	}
+	
+	private void setStudent() {
+		
+	}
+	
+	public void setDialogStage(Stage dialogStage) {
+		this.dialogStage = dialogStage;
+	}
+	
+	public void closeWindow() {
+		dialogStage.close();
+	}
 
 }
