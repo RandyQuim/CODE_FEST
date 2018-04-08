ee0b1fdcd8d7fcef9f9c78b0f0567d84f74a24e8
Chris Ault <cda17@students.uwf.edu>
2018-04-07 21:36:12 -0500

    Teacher Admin updated

--- Neural Pathways/src/controller/TeacherAdministration.java
+++ Neural Pathways/src/controller/TeacherAdministration.java
@@ -15,11 +15,23 @@ public class TeacherAdministration {
 	public ArrayList<Student> getStudents(){
 		return students;
 	}
-	
+	public ArrayList<String> getStudentsNames(){
+		ArrayList<String> temp = new ArrayList<String>();
+    	for (int i = 0; i < 3; i++) {
+    		temp.add("Johnney");
+    	}
+		return temp;
+	}	
 	public ArrayList<Trivia> getQuizzes(){
 		return quizzes;
 	}
-	
+	public ArrayList<Integer> getQuizID(){
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+    	for (int i = 0; i < 3; i++) {
+    		temp.add(i);
+    	}
+		return temp;
+	}	
 	
 	public ArrayList<Step> getMistakes(int student) {
 		return students.get(student).getIncorrectSteps();
